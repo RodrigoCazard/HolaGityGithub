56fd6f1c125314a67e4ed0807c55aa167448075d
Rodrigo Cazard <108705909+RodrigoCazard@users.noreply.github.com>
2026-02-14 20:40:29 -0300

    El primer commit es mio, Rodrigo

---
+++ HolaGityGithub.java
@@ -0,0 +1,11 @@
+public class HolaGitHub {
+
+    public static void main(String[] args) {
+
+        // Creen lineas debajo poniendo un mensaje y poniendo quien lo escribio
+
+        String mensaje = "Hola, este es mi primer cambio en el repositorio!";
+
+        System.out.println(mensaje);
+    }
+}
